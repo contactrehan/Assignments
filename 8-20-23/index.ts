fa2ac8005a469be3ded666399d934efefd2e3bf7
contactrehan <contactrehan2209@gmail.com>
2023-08-22 16:15:46 +0500

    8-2-23

---
+++ 8-20-23/index.ts
@@ -0,0 +1,64 @@
+// var i:number =1
+// while (i<=10) 
+// {
+// console.log(i)
+// i++    
+// }
+// var i:number = 1
+// do{
+//     console.log('Rehan');
+//     i++;
+// }while(i<10)
+//write a ts program that calculates sum of even numbers from 1 and 100 and shows the added number
+// var evenNumber:number = 1
+// let sum :number =0
+// while (evenNumber<=100){
+
+//   evenNumber++
+//   console.log(evenNumber)
+//   if((evenNumber%2)==0){
+//     sum=sum+evenNumber
+// }
+// }
+// console.log(sum)
+// var i:number = 1
+// var sum:number = 0
+// while (i<100) {
+//     i++
+//     console.log(i)
+
+//     if (i%2==0) 
+//     {
+        
+//         sum = sum + i
+        
+//     }
+    
+// }
+// console.log(sum)
+// create a function that takes an array of numbers as parameters,
+//  use a while loop to calculate and return the sum of all the numbers in the array
+// var sum:number = 0
+// function arrayFunction(arrayNumbers:number[]) 
+// {
+//     while (sum>=0)
+//     {
+//         sum=arrayNumbers.length
+// sum = 
+// console.log(sum)
+// return sum
+//     }
+    
+// }
+// arrayFunction()
+var i:number = 1
+function Factorial(num1:number) {
+    if (num1>0)
+     {console.log("Number is Positive")
+    //  i= num1
+    while(num1>)
+     var result = num1*(num1-i)  
+
+    }
+    
+}
