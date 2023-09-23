145c6b5f1cc77abd9f6e1c3f241e13e9185f36f4
contactrehan <contactrehan2209@gmail.com>
2023-09-23 07:20:25 +0500

    -A

---
+++ 9-18-23/index.ts
@@ -0,0 +1,9 @@
+/* CALCULTOR application which takes input from user via inquirer and perform 
+ - Addition
+ - Subtraction
+ - Multiplication
+ - Modulus
+ - Division
+ - Exponent
+*/
+import inquirer from "inquirer"
